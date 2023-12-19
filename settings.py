d89cb8fa3e921d83c88c3431454726452abb6247
Skriwel <maks6501@gmail.com>
2023-12-19 21:16:31 +0200

    1

---
+++ settings.py
@@ -0,0 +1,8 @@
+token = 'MTE3NDA3MjA0MzczNDU3MzA1Ng.GnNjn_.91LzAn5ZsoS5cg-Qa6ribjnt7pToiUJqj3vRMQ' #токен бота
+
+prefix = '/' #префикс команды
+
+channelid = 995727306809151528 # скажем новеньким привет
+channelhi = 946083077740957707 # прихожка
+role_rec = 880050661398556722 #Роль рекрутёра 
+
